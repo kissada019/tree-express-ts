46729969b54d8bd33e84d3a81320f4819c834218
kissada <kissada@attaplus.com>
2026-06-24 11:19:16 +0700

    chore: update deployment compose workflow

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 echo "Hello Jenkins"
+
 rm -rf tree-express-ts
 git clone https://github.com/kissada019/tree-express-ts.git
-echo "successfully clone tree-express-ts"
+
+cd tree-express-ts
+
 echo "build image and starting container"
-docker-compose -f docker-compose.prod.yml down
-docker-compose -f docker-compose.prod.yml build --no-cache
-docker-compose -f docker-compose.prod.yml up -d
+
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
